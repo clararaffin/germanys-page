a47556b8699690603f1e68e0dd932c14aced2471
Arias-R <rama_arias@hotmail.es>
2015-07-01 16:15:29 -0300

    Update buybook.php

--- buybook.php
+++ buybook.php
@@ -4,9 +4,11 @@ $title = @$_GET['nombre'];
 if ($title){
 	$registro = mysql_query("SELECT * FROM libros WHERE titulo='".@$_GET['nombre']."'" ) ;
 	while($reg=mysql_fetch_array($registro)){
-	echo "<br>",$reg['titulo'] ;
-	echo "<br>",$reg['autor'] ;
-	echo "<br>",$reg['descripcion'] ;
+	$newcant=$reg['cantidad'];
 	}
+	$newcant=$newcant-1;
+	echo $newcant;
+	mysql_query("UPDATE libros SET cantidad='".$newcant."' WHERE titulo='".$title."'");
+	
 }
 ?>
