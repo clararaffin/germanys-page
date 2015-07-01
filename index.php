80e43eaa5e6f956f6b25edf66223c44077b804e3
Arias-R <rama_arias@hotmail.es>
2015-07-01 11:33:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
+<!DOCTYPE html>
+<head>
+	<title>Mostrar todos los libros</title>
+</head>
+<body>
+	
+</body>
+
 <?php
-
-//Aca iria el inicio de la pagina
-
+require ('config.php');
+$registro = mysql_query("SELECT * FROM libros") or  die ("No se encontro la base con los libros");
+while($reg=mysql_fetch_array($registro)){
+	echo "<br>",$reg['titulo'] ;
+	echo "<br>",$reg['autor'] ;
+	echo "<br>",$reg['categoria'] ;
+	echo "<br>",$reg['descripcion'] ;
+	echo "<br>",$reg['cantidad'] ;
+}
 ?>
+<html>
+
+</html>
