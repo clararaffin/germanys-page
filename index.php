943c16efcf11cbd22ca447a4872898674871465b
sebamoralesasd <sebasuni96@gmail.com>
2015-07-02 11:10:57 -0300

    +

--- index.php
+++ index.php
@@ -29,22 +29,20 @@ body {
 include_once ('header.php');
 
 ?>
-
-      <div id="myTabContent" class="tab-content">
-        <div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledBy="home-tab">      
-          <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
+<div id="myTabContent" class="tab-content">
+	<div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledBy="home-tab">
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
               <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              
+              <li data-target="#myCarousel" data-slide-to="1"></li>              
             </ol>
+
             <div class="carousel-inner" role="listbox">
-              <div class="item active">
+            	<div class="item active">
                 <img class="first-slide" src="hg1.jpg" alt="First slide">
                 <div class="container">
                   <div class="carousel-caption">
-                    <p><a class="btn btn-lg btn-info" href="#" role="button">Más</a></p>
+                    <p><a class="btn btn-lg btn-info" href="#" role="button">Comprar</a></p>
                   </div>
                 </div>
               </div>
@@ -52,12 +50,12 @@ include_once ('header.php');
                 <img class="second-slide" src="hg2.jpg" alt="Second slide">
                 <div class="container">
                   <div class="carousel-caption">
-                    <p><a class="btn btn-lg btn-info" href="#" role="button">Más</a></p>
+                    <p><a class="btn btn-lg btn-info" href="#" role="button">Comprar</a></p>
                   </div>
                 </div>
               </div>
-              
             </div>
+
             <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
               <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
               <span class="sr-only">Previous</span>
@@ -66,8 +64,10 @@ include_once ('header.php');
               <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
               <span class="sr-only">Next</span>
             </a>
-          </div><!-- /.carousel -->
-         
+
+		</div>
+	</div>
+</div>
 
 
 <?php 
